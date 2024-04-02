e6a6fbf020743a41ae6fd6094af519ed06e3fdb7
Jorge Huete <jorgehuetes@gmail.com>
2024-04-02 16:13:14 -0700

    First commit

---
+++ animations.py
@@ -0,0 +1,147 @@
+import imageio.v2 as imageio
+from PIL import Image
+
+
+
+# Animation config, to be moved to a config file
+BACKGROUND_FILE = "background.png" # Background image, shared by all animations
+LAYERS = ["face", "mouth", "eyes", "flair"] # Layers to be added, shared by all animations
+"""
+This should be something like:
+face: blush, hair, nose, anything not moving
+mouth: mouth, may change based on voice or something
+eyes: eyes, change automatically based on time to simulate blinking
+flair: anything that is not part of the face, like special effects
+Animations should have layers matching the names in this list
+"""
+
+# IDLE ANIMATION
+IDLE = {"face": "idle_face.png", "mouth": "idle_mouth.png", "eyes": ["idle_eyes.png", "idle_blink.png"], "flair": None}
+MAD = {"face": "idle_face.png", "mouth": "mad_mouth.png", "eyes": "mad_eyes.png", "flair": None}
+
+def generate_image(layers: list, working_resolution: tuple):
+    """Gets a layers list and generates the final image
+    Simply alpha composites the layers on top of each other in the order they are given
+    Resizes the layers to the working resolution
+
+    input: layers dictionary
+    output: final image
+    
+    """
+    final_image = Image.new("RGBA", working_resolution, (0, 0, 0, 0))
+    for layer in layers:
+        # If it is already a PIL image, no need to convert it
+        if isinstance(layer, Image.Image):
+            final_image = Image.alpha_composite(final_image, layer.resize(working_resolution))
+        else:
+            final_image = Image.alpha_composite(final_image, Image.fromarray(layer).resize(working_resolution))
+
+    return final_image
+
+
+def get_static_first_frame(ressource_path, working_resolution):
+    """Get the first frame of a static animation
+    This should be used when first instantiating the window to have something to display
+    Automatically creates an open eye frame of the idle animation
+    """
+    layer_arrays = []
+    layer_arrays.append(Image.open(ressource_path + BACKGROUND_FILE).resize(working_resolution))
+    for layer in LAYERS:
+        filename = IDLE[layer]
+        # If its a list, get the first element
+        if isinstance(filename, list):
+            filename = filename[0]
+        if filename is None:
+            continue
+        # Create a PIL image from the file and resize it
+        img = Image.open(ressource_path + filename)
+        layer_arrays.append(img.resize(working_resolution))
+
+    return generate_image(layer_arrays, working_resolution)
+        
+def load_layer_images(ressource_path, layer_dict, working_resolution):
+    """Helper function to load and resize images for each layer"""
+    layer_images = {}
+    for layer, filename in layer_dict.items():
+        if filename is None:
+            continue
+        if isinstance(filename, list):
+            sub_array = []
+            for sub in filename:
+                sub_array.append(Image.open(ressource_path + sub).resize(working_resolution))
+            layer_images[layer] = sub_array
+        else:
+            layer_images[layer] = Image.open(ressource_path + filename).resize(working_resolution)
+    return layer_images
+
+class FrameGenerator:
+    """A class to generate frames based on multiple posible animations
+        Hold state accross frames as a sort of memory for the animations
+        
+    """
+    def __init__(self, working_resolution, ressource_path):
+        self.current_state = "idle"
+        self.current_frame = 0
+        self.current_frame_time = 0
+        self.current_animation = None
+        self.animations = {"idle": self.idle_animation, "mad": self.mad_animation}
+
+        self.working_resolution = working_resolution
+        self.background = Image.open(ressource_path + BACKGROUND_FILE).resize(working_resolution)
+
+
+        # Idle animation initialization
+        self.idle_layers = load_layer_images(ressource_path, IDLE, working_resolution)
+        self.idle_eyes_state = 0 # 0 is open, 1 is closed
+        self.next_change = 0 # Time to change the eyes state
+        self.blink_delay = 60
+        self.blink_duration = 5
+        self.idle_animation_buffer = None
+
+        # Mad animation initialization
+        self.mad_animation_buffer = None
+        self.mad_layers = load_layer_images(ressource_path, MAD, working_resolution)
+        
+
+    def execute_animation(self):
+        """Execute the current animation
+        """
+        output = self.animations[self.current_state]()
+        self.current_frame += 1
+
+        return output
+
+    def idle_animation(self):
+        """Idle animation
+        """
+        if self.idle_animation_buffer is None:
+            # Generate the first frame by assuming the eyes are open
+            layer_arrays = [self.background, self.idle_layers["face"], self.idle_layers["mouth"], self.idle_layers["eyes"][0]]
+            self.idle_animation_buffer = generate_image(layer_arrays, self.working_resolution)
+        else:
+            # Do we need to change the eyes state?
+            if self.current_frame >= self.next_change:
+                
+                # Change the eyes state
+                if self.idle_eyes_state == 0:
+                    self.idle_eyes_state = 1
+                    self.next_change = self.current_frame + self.blink_duration
+                else:
+                    self.idle_eyes_state = 0
+                    self.next_change = self.current_frame + self.blink_delay
+
+                # Generate the new frame
+                layer_arrays = [self.background, self.idle_layers["face"], self.idle_layers["mouth"], self.idle_layers["eyes"][self.idle_eyes_state]]
+                self.idle_animation_buffer = generate_image(layer_arrays, self.working_resolution)
+
+        return self.idle_animation_buffer
+            
+        
+
+    def mad_animation(self):
+        """Mad animation
+        """
+        if self.mad_animation_buffer is None:
+            layer_arrays = [self.background, self.mad_layers["face"], self.mad_layers["mouth"], self.mad_layers["eyes"]]
+            self.mad_animation_buffer = generate_image(layer_arrays, self.working_resolution)
+        return self.mad_animation_buffer
---
+++ main.py
@@ -0,0 +1,131 @@
+import os
+import imageio.v2 as imageio
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image, ImageTk
+import threading
+import time
+import tkinter as tk
+import animations as anim
+
+# Path to the bitmaps folder
+BITMAPS_PATH = "bitmaps/"
+
+# Layers names array
+LAYER_NAMES = ["background", "static_face", "mouth", "eyes"]
+layer_filenames = {"background": "background.png", "static_face": "face.png", "mouth": "mouth.png", "eyes": ["eyes1.png", "eyes2.png"]}
+layers = {}
+
+# GUI globals
+TARGET_FRAMERATE = 30
+update_delay = 1/TARGET_FRAMERATE
+working_resolution = None
+output_object = None
+output_buffer = None
+label = None
+
+
+def state_machine(animation):
+    """A simple state machine to control the animation
+    """
+    while True:
+        # Every 10 seconds, change the state from IDLE to MAD or vice versa
+        if animation.current_state == "idle":
+            animation.current_state = "mad"
+            time.sleep(3)
+        else:
+            animation.current_state = "idle"
+            time.sleep(7)
+
+
+
+def child_thread():
+    global output_object
+    global output_buffer
+
+    animation = anim.FrameGenerator(working_resolution=working_resolution, ressource_path=BITMAPS_PATH)
+    # Start the state machine
+    thread = threading.Thread(target=state_machine, args=(animation,))
+    thread.daemon = True
+    thread.start()
+
+    # Tick
+    start = time.time()
+    while True:
+        target_time = time.time() + update_delay
+
+        ### FRAME GENERATION START
+        frame = animation.execute_animation()
+        output_buffer = ImageTk.PhotoImage(frame)
+        output_object.configure(image=output_buffer)
+        output_object.image = output_buffer
+
+        ### FRAME GENERATION END
+
+        # Sleep by the remaining time)
+        while time.time() < target_time:
+            pass
+        
+
+
+def blink_delay_humanize(open_base=3, close_base=0.1, open_variance=0.5, close_variance=0.05):
+    """Generates a random delay time for the blinking of the eyes
+    The delay time is generated using a normal distribution with the given parameters
+
+    input: open_base, close_base, open_variance, close_variance
+    output: delay times for the eyes [open_delay, close_delay]
+    """
+    # Generate two random numbers for the open and close delays
+    open_delay = np.random.normal(open_base, open_variance)
+    close_delay = np.random.normal(close_base, close_variance)
+    # Return the delay times
+    return [max(0, open_delay), max(0, close_delay)]
+
+
+
+
+
+def main():
+    """Main function
+    Intializes the GUI by filling global parameters of the full screen window
+    Starts the child thread for updating the image
+    Hands over the control to the tkinter main loop
+    
+    """
+    global output_object
+    global output_buffer
+    global label
+    global working_resolution
+    
+    # Plot the image using tk
+    root = tk.Tk()
+    root.title("A pretty face")
+    root.attributes("-fullscreen", True)
+
+    # Fill the global working resolution with the full screen resolution
+    working_resolution = (root.winfo_screenwidth(), root.winfo_screenheight())
+
+
+    # Prepare a first image to display
+    output_buffer = anim.get_static_first_frame(BITMAPS_PATH, working_resolution)
+    img = ImageTk.PhotoImage(output_buffer)
+
+    # Fullscreen output object, shared with the child thread for updating
+    output_object = tk.Label(root, image=img)
+    output_object.pack(fill="both", expand=True)
+
+    # Start the child thread before the main loop
+    thread = threading.Thread(target=child_thread)
+    thread.daemon = True
+    thread.start()
+
+    # Hand over the control to the tkinter main loop
+    root.mainloop()
+
+    # If the main loop is exited, kill the child thread
+    return 0
+
+
+if __name__ == "__main__":
+    main()
